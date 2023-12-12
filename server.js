7a63e32b1b36fbb43680a4fda7b28f5160ec2242
aniruth-gundawar <aniruthgundawar@gmail.com>
2023-12-13 01:26:38 +0530

    code Added

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import colors from "colors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import bookRoutes from "./routes/bookRoute.js";
+
+const path = require("path");
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//api end point
+app.use("/api", bookRoutes);
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "LMS Website",
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`.bgCyan.white);
+});
